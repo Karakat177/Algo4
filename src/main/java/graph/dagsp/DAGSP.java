688d9d3a0f63ef2238a25ac9e63ba1ed6557ae7f
Karakat177 <ibraimkarakat07@gmail.com>
2025-11-03 02:12:55 +0500

    Update DAGSP.java

--- src/main/java/graph/dagsp/DAGSP.java
+++ src/main/java/graph/dagsp/DAGSP.java
@@ -16,10 +16,10 @@ public class DAGSP {
         for(int u: topo){
             if(dist[u]==INF) continue;
             for(Edge e: g.adj.get(u)){
-                metrics.incOps(); // проверка обновления расстояния
+                metrics.incOps();
                 if(dist[u]+e.weight < dist[e.to]){
                     dist[e.to] = dist[u]+e.weight;
-                    metrics.incOps(); // обновление расстояния
+                    metrics.incOps();
                 }
             }
         }
@@ -28,32 +28,3 @@ public class DAGSP {
     }
 }
 
-
-
-/**package graph.dagsp;
-
-import graph.Graph;
-import graph.Edge;
-import java.util.*;
-
-public class DAGSP {
-
-    public static int[] shortestPath(Graph g, List<Integer> topo, int start){
-        int n = g.n;
-        int INF = 1_000_000_000;
-        int[] dist = new int[n];
-        Arrays.fill(dist, INF);
-        dist[start]=0;
-
-        for(int u: topo){
-            if(dist[u]==INF) continue;
-            for(Edge e: g.adj.get(u)){
-                if(dist[u]+e.weight<dist[e.to]){
-                    dist[e.to] = dist[u]+e.weight;
-                }
-            }
-        }
-        return dist;
-    }
-}
-*/
