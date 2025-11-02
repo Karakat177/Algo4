0a280c1edb76188aadd46c26bc0d9a3c9cdaebd4
Karakat177 <ibraimkarakat07@gmail.com>
2025-11-03 02:11:13 +0500

    Update TopologicalSortKahn.java

--- src/main/java/graph/topo/TopologicalSortKahn.java
+++ src/main/java/graph/topo/TopologicalSortKahn.java
@@ -35,33 +35,3 @@ public class TopologicalSortKahn {
     }
 }
 
-
-/**package graph.topo;
-
-import graph.Graph;
-import java.util.*;
-
-public class TopologicalSortKahn {
-
-    public static List<Integer> sort(Graph g){
-        int n = g.n;
-        int[] inDeg = new int[n];
-        for(var edges: g.adj){
-            for(var e: edges) inDeg[e.to]++;
-        }
-        Deque<Integer> q = new ArrayDeque<>();
-        for(int i=0;i<n;i++) if(inDeg[i]==0) q.add(i);
-
-        List<Integer> order = new ArrayList<>();
-        while(!q.isEmpty()){
-            int u = q.poll();
-            order.add(u);
-            for(var e: g.adj.get(u)){
-                inDeg[e.to]--;
-                if(inDeg[e.to]==0) q.add(e.to);
-            }
-        }
-        return order;
-    }
-}
-*/
